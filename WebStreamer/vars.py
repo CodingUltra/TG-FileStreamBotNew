413c4c49b6311e660f33b5306276077cd67bdb56
CodingUltra <128906681+CodingUltra@users.noreply.github.com>
2023-05-03 17:28:53 +0530

    Update vars.py

--- WebStreamer/vars.py
+++ WebStreamer/vars.py
@@ -26,7 +26,7 @@ class Var(object):
     HASH_LENGTH = int(environ.get("HASH_LENGTH", 6))
     if not 5 < HASH_LENGTH < 64:
         sys.exit("Hash length should be greater than 5 and less than 64")
-    FQDN = str(environ.get("FQDN", BIND_ADDRESS))
+    FQDN = str(environ.get("FQDN", 52.66.246.178:8080))
     URL = "http{}://{}{}/".format(
             "s" if HAS_SSL else "", FQDN, "" if NO_PORT else ":" + str(PORT)
         )
